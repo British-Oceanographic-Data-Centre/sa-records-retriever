84ac9312c050d25ab530983e555c3cb0ddca5018
Rob Jennings <roje@bodc.ac.uk>
2025-02-11 16:31:25 +0000

    fix dir batching

--- retriever/retrieve.py
+++ retriever/retrieve.py
@@ -55,7 +55,7 @@ if __name__ == "__main__":
             rec_no = offset + batch_rec_counter
             print(f"written rec. no {rec_no}")
 
-            if (offset + rec_no - dir_size) >= offset:
+            if rec_no >= dir_size:
                 dir_count += 1
                 dir_name = base_dir / f"records_{str(dir_count).zfill(6)}"
                 Path.mkdir(dir_name, exist_ok=True)
